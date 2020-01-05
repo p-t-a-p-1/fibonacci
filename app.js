63f3e7d534c23a8b24ca39df41d27774e1203f1e
p-t-a-p-1 <kb240225@gmail.com>
2020-01-05 22:48:15 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+/**
+ * フィボナッチ数列 f(n) = f(n-1) + f(n-2)
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+ */
 'use strict'
+// Map作成
+const memo = new Map()
+// 0, 1はデフォルト値
+memo.set(0, 0)
+memo.set(1, 1)
+
+/**
+ * Map関数を用い、フィボナッチ数列の計算を行う
+ * @param int n
+ */
 function fib(n) {
-  if (n === 0) {
-    return 0
-  } else if (n === 1) {
-    return 1
+  if (memo.has(n)) {
+    return memo.get(n)
   }
-  return fib(n - 1) + fib(n - 2)
+  const value = fib(n - 1) + fib(n - 2)
+  memo.set(n, value)
+  return value
 }
 const length = 40
 for (let i = 0; i <= length; i++) {
